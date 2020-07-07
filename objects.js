0034574bda86e3325589d5bd4c617eb3a337bffa
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-07-07 19:42:24 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -12,8 +12,13 @@ var art = {Bruno: Treasure}
   updatePlaylist(art, 'floor','HAD ENOUGH' )
 
 function removeFromPlaylist(playlist, artistName){
+<<<<<<< HEAD
 
   delete playlist[artistName];
   return playlist
+=======
+  playlist = delete playlist.artistName;
+
+>>>>>>> 759c3d8d41181c9538d67de337af815aa686604a
 }
 removeFromPlaylist()
